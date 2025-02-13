ed9b2cd79128071e720107ee4751118a4794db09
Muhammad Rizki <10210045@gmail.com>
2025-02-13 12:35:38 +0700

    add smting

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime';
+import './styles/style.css';
+import './styles/output.css';
+import App from './views/app';
+const app = new App({
+    // button: document.querySelector('#hamburgerButton'),
+    // drawer: document.querySelector('#navigationDrawer'),
+    content: document.querySelector('#mainContent'),
+  });
