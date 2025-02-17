7097e5e373077be83db9b39d7c1119caf2a4c7d9
Muhammad Rizki <10210045@gmail.com>
2025-02-17 14:41:05 +0700

    test fitur

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,10 @@
 import 'regenerator-runtime';
+import $ from 'jquery';
 import './styles/style.css';
 import './styles/output.css';
 import App from './views/app';
 const app = new App({
-    // button: document.querySelector('#hamburgerButton'),
-    // drawer: document.querySelector('#navigationDrawer'),
-    content: document.querySelector('#mainContent'),
-  });
+  // button: document.querySelector('#hamburgerButton'),
+  // drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
